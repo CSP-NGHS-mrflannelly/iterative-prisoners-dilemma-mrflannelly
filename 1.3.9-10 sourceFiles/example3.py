7441e9489d361b19212cac4934b9d5a0432e81bd
Chandra132 <anthony.achtziger@ngsd.k12.wi.us>
2016-11-10 13:33:16 -0600

    Revert "Changed Team Name and Summary"
    This reverts commit c1420f8bc3f7974d2a5e2caad29e58cb28d0a34f.

--- 1.3.9-10 sourceFiles/example3.py
+++ 1.3.9-10 sourceFiles/example3.py
@@ -6,11 +6,11 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The Destroyers'
+team_name = 'E3'
 strategy_name = 'Collude but retaliate'
 strategy_description = '''\
 Collude first round. Collude, except in a round after getting 
-a severe punishment. Destroy all things.'''
+a severe punishment.'''
     
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
