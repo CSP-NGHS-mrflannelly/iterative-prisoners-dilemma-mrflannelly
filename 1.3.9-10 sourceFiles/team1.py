026668bdc449139dea363b035f3f040d5c6336a5
Robex1 <Robert.chenoweth@ngsd.k12.wi.us>
2016-11-11 13:33:23 -0600

    Changed names, stradegy
    We added the team name and team stradegy and added to stradegy

--- 1.3.9-10 sourceFiles/team1.py
+++ 1.3.9-10 sourceFiles/team1.py
@@ -6,18 +6,26 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'RobandReiswillRek' # Only 10 chars displayed.
+strategy_name = 'Stradegy Reader'
+strategy_description = 'the other team will do things based on our previous performance so be unpredictable in our random pattern basing off what they choose. The first 4 will be every other. Next we will read their first things to find out their algorithm and go accordingly.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
-    
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(my_history) <= 4:
+        if len(my_history)%2 == 0:
+            return'b'
+        else:
+            return'c'
+    else:
+        if their_history.lower() == 'bcbc' 
+        
+        
+        
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
